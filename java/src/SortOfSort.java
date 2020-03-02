acf472747c45f1ae756f88dd778acec2cb3be031
alan <agamez8@miners.utep.edu>
2020-03-02 14:46:32 -0700

    sortOfSort method started

--- java/src/SortOfSort.java
+++ java/src/SortOfSort.java
@@ -1,2 +1,17 @@
 public class SortOfSort {
+    public static void main(String []args){
+        int[] array = {2, 7, 1, 3, 0, 9, 6, 5};
+        int[] nums;
+    }
+
+    public void sortOfSort(int[] array1){
+        int sum = 0;
+        for(int i = 0; i < array1.length; i++){
+
+        }
+    }
+
+    public int m(int[] arr){
+        return 0;
+    }
 }
