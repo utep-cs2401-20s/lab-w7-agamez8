6da2b0bd044975b932e4d36a2d2a1b2f6140fa2d
alan <agamez8@miners.utep.edu>
2020-03-04 14:47:43 -0700

    method continued

--- java/src/SortOfSort.java
+++ java/src/SortOfSort.java
@@ -1,17 +1,39 @@
 public class SortOfSort {
-    public static void main(String []args){
-        int[] array = {2, 7, 1, 3, 0, 9, 6, 5};
-        int[] nums;
+    public static void main(String[] args) {
+        int[] array1 = {2, 7, 1, 3, 0, 9, 6, 5};
+        //sortOfSort(array1);
     }
 
-    public void sortOfSort(int[] array1){
-        int sum = 0;
-        for(int i = 0; i < array1.length; i++){
+    public void sortOfSort(int[] array) {
+        if (array.length <= -1)
+            return;
 
+        //variables
+        int temp;
+        int highIn = array.length - 1;
+        int lowIn = 0;
+        int counter = 0;
+        int max;
+
+        //iterate through array
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                if (array[i] > array[j]) {
+                    temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                    max = array[j];
+                }
+            }
+            if(highIn > lowIn){
+                if(counter < 2){
+                    temp = array[highIn];
+                    array[highIn] = max;
+
+                }
+            }
         }
     }
-
-    public int m(int[] arr){
-        return 0;
-    }
 }
+
+
